880480f006abd77b555506fa76262f957e238b67
Luis-Brandao <26489805+Luis-Brandao@users.noreply.github.com>
2018-12-08 18:07:24 +0000

    Update typeChecker.hs

--- Ltypes/src/typeChecker.hs
+++ Ltypes/src/typeChecker.hs
@@ -1,29 +1,28 @@
+
 data Type = TypeArrow Multiplicity Type Type | TBoolean deriving (Eq,Show)
 
-data Multiplicity = One | Omega deriving (Show,Eq)
+data Multiplicity = One | Omega deriving Show
 
 instance Num Multiplicity where
   _ + b   = Omega
   One * b = b
   b * One = b
   _ * _ = Omega
-  fromInteger 1 = One
-  fromInteger _ = Omega
+  fromInteger n = if (n == 1) then One else Omega
 
-instance Ord Multiplicity where
-  compare w 1 = GT
-  compare 1 w = LT
+instance Eq Multiplicity where
+  1 == 1 = True
+  1 == Omega = False
+  Omega == 1 = False
+  Omega == Omega = True
 
 type Bind = (Name, Multiplicity, Type)
 
 type Context = [Bind]
 
 type Name = String
-type Tag = String
 
-type Alt = (Tag, [Name], Term)
-
-data Term = Var Name | Lambda Bind Term | App Term Term | Case Term [Alt] | Contructor Tag [Term]
+data Term = Var Name | Lambda Bind Term | App Term Term
 
 type Tc a = Either String a
 
@@ -36,8 +35,12 @@ scaleCtx :: Multiplicity -> Context -> Context
 scaleCtx x ctx = [(name, (m*x)
   , ty)|(name, m, ty) <- ctx]
 
+
 addCtx :: Context -> Context -> Context
-addCtx ctx1 ctx2 = [case x `look` ctx2 of Just (x',q',t') -> (x, q+q', t) ; Nothing -> (x,q,t) | (x,q,t)<-ctx1]
+addCtx ctx1 [] = ctx1
+addCtx [] ctx2 = ctx2
+addCtx ((x,m,t) : ctx1) ctx2 | (x,m,t) `bindIn` ctx2 = (x, Omega, t) : addCtx ctx1 (deleteBind x ctx2)
+                              | otherwise = (x,m,t) : addCtx ctx1 ctx2
 
 throw :: String -> Tc a
 throw message = Left message
@@ -49,63 +52,24 @@ look lookUpName ((name,m,ty):tail) = if(lookUpName == name) then (Just (name,m,t
 deleteBind :: Name -> Context -> Context
 deleteBind lookUpName ctx = filter (\(x,q,ty)->x/=lookUpName) ctx
 
-identity  = (Lambda ("x",1,TBoolean) (Var "x"))
+check :: Term -> Tc (Context, Type)
+check (Var x) = let ctx = [(x,1,TBoolean)] in return (ctx,TBoolean)
+check (Lambda (x,q,ty1) term ) =  do (ctx,ty2) <- check term;
+                                      let result = TypeArrow q ty1 ty2 in
+                                      case look x ctx of 
+                                        Just (x',q',ty') -> 
+                                          if (q == q') then
+                                            if (ty1 == ty') then 
+                                              return ((deleteBind x ctx),result) 
+                                            else 
+                                              throw ("Mismatched type. Expected" ++ show ty1 ++ "but found" ++ show ty')
+                                          else throw "Wrong Multiplicity"
+                                        Nothing -> if(q == Omega) then return (ctx,result) else throw "type error"   
+                                  
+                                  
 
-identity' = (Lambda ("x",Omega,TBoolean) (Var "x"))
-
-discard   = Lambda("y",Omega, TBoolean) (Lambda ("x",1
-
-  ,TBoolean) (Var "y"))
-
-app = (Lambda("foo",Omega, (TypeArrow Omega TBoolean TBoolean)))(Lambda("y",Omega, TBoolean)(App (Var "foo") (Var "y")))
-
---Advanced Topics in Types in Programming Languages Benjamin C. Pierce
-
---Má ideia definir a compatibilidade assim, caso a multiplicidade seja expandida
-
-compatible :: Bind -> Context -> Bool
-compatible (x,q,t) gama = case (x `look` gama) of Just (x',q',t') -> ((q `compatibleMult` q') && (t == t'))
-                                                  Nothing          -> q == Omega
-
-
-compatibleMult :: Multiplicity -> Multiplicity -> Bool
-compatibleMult 1 1     = True
-compatibleMult Omega _ = True
-compatibleMult _ _     = False
-
-
-checkTop :: Term -> Tc (Context, Type)
-checkTop t = check [] t
-
-check :: Context -> Term -> Tc (Context, Type)
-check ctxIn (Var x) = case (look x ctxIn) of
-                        Just (_,_,ty) -> return ([(x,1,ty)],ty)
-                        Nothing       -> throw "Non defined var"
-
---    Gama,x:qA |-c t:B ; Gama' x:qA compatible Gama'
--- _________________________________________________ (ABS)
---
---     Gama |-c Lambda (x:qA) . t:A ->q B ; Gama'-x                                  
-check ctxIn (Lambda (x,q,a) t ) =  do{
-  (ctxOut,b) <- check ((x,q,a):ctxIn) t;
-  let result = (TypeArrow q a b) in
-  case look x ctxOut of 
-    Just (x',q',t') -> if (x',q',t') `compatible` ctxOut then return (( x `deleteBind` ctxOut),result) else throw "type error"
-    Nothing -> if(q == Omega) then return (ctxOut,result) else throw (x ++ " was discarded")
-}                              
-
---        Gama |-c A ->q B ; Gama'   Gama |- u:A ; Delta
---  _______________________________________________________(APP)
---
---           Gama |-c t.u :B ; Gama' + q*Delta                         
-check ctxIn (App t u) = do{ 
-                            (_,(TypeArrow q a b)) <- check ctxIn t;
-                            (gamma,type2)         <- check ctxIn u;
-                            let result = ctxIn `addCtx` (q `scaleCtx` gamma) in
-                              if(a == type2) then return (result, b) else throw "type error"                              
-                          }
-
-check _ _ = throw "pattern matching exausted"
-
-
-
+                                 
+check (App term1 term2) = do (ctx1,(TypeArrow q a b)) <- check term1;
+                              (ctx2,type2) <- check term2;
+                              let ctx = ctx1 `addCtx` (scaleCtx q ctx) in
+                                if(a == type2) then return (ctx, b) else throw "type error"
